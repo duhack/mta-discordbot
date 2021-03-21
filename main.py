03a98c7db5e87858c21661f2bb15ab97130990e4
duhack <>
2021-03-21 10:32:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+import config
+
+intents = discord.Intents.all()
+TOKEN = config.configCheck('token')
+bot = commands.Bot(command_prefix=config.configCheck('prefix'), intents=intents)
+
+extensions = ['commands', 'events']
+
+for extension in extensions:
+    bot.load_extension(extension)
+
+bot.run(TOKEN)
